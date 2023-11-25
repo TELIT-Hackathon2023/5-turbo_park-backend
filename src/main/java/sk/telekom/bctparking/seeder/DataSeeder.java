1e851258a5eea45b816920f25b6d964fb27bdf93
mh337km <maria.hrehova@student.tuke.sk>
2023-11-25 22:46:15 +0100

    data

--- src/main/java/sk/telekom/bctparking/seeder/DataSeeder.java
+++ src/main/java/sk/telekom/bctparking/seeder/DataSeeder.java
@@ -3,9 +3,14 @@ package sk.telekom.bctparking.seeder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import sk.telekom.bctparking.model.Employee;
 import sk.telekom.bctparking.model.ParkingSlot;
+import sk.telekom.bctparking.model.Ticket;
+import sk.telekom.bctparking.repository.EmployeeRepository;
 import sk.telekom.bctparking.repository.ParkingSlotRepository;
+import sk.telekom.bctparking.repository.TicketRepository;
 
+import java.time.OffsetDateTime;
 import java.util.Arrays;
 
 @Component
@@ -13,6 +18,10 @@ import java.util.Arrays;
 public class DataSeeder implements CommandLineRunner {
 
     private final ParkingSlotRepository parkingSlotRepository;
+
+    private final EmployeeRepository employeeRepository;
+
+    private final TicketRepository ticketRepository;
 
     private void generateData() {
         ParkingSlot parkingSlot1 = new ParkingSlot()
@@ -43,6 +52,36 @@ public class DataSeeder implements CommandLineRunner {
 
         parkingSlotRepository.saveAll(Arrays.asList(parkingSlot1, parkingSlot2, parkingSlot3,
                 parkingSlot4, parkingSlot5));
+
+        Employee employee1 = new Employee()
+                .setName("Bob")
+                .setSurname("Bobers")
+                .setEmail("bob@example.com")
+                .setPassword("BobsPa$$w0rd")
+                .setPhoneNumber("+421000000000")
+                .setPersonalId(123456L)
+                .setLicencePlateNumber("KE456RI");
+        Employee employee2 = new Employee()
+                .setName("Johan")
+                .setSurname("Dears")
+                .setEmail("johan@example.com")
+                .setPassword("JohansPa$$w0rd")
+                .setPhoneNumber("+421000000123")
+                .setPersonalId(78946L)
+                .setLicencePlateNumber("KE951DE");
+        employeeRepository.saveAll(Arrays.asList(employee1,employee2));
+
+        Ticket ticket1 = new Ticket()
+                .setEmployee(employee1)
+                .setParkingSlot(parkingSlot1)
+                .setStartDate(OffsetDateTime.now())
+                .setEndDate(OffsetDateTime.now().plusHours(1));
+        Ticket ticket2 = new Ticket()
+                .setEmployee(employee2)
+                .setParkingSlot(parkingSlot1)
+                .setStartDate(OffsetDateTime.now().plusHours(1))
+                .setEndDate(OffsetDateTime.now().plusHours(2));
+        ticketRepository.saveAll(Arrays.asList(ticket1,ticket2));
     }
 
     @Override
