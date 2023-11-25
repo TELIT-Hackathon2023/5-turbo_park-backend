9fa722a5fdee55ba15e8b2eac3c84bbc35f08ba2
mh337km <maria.hrehova@student.tuke.sk>
2023-11-25 18:12:00 +0100

    Tickets

--- src/main/java/sk/telekom/bctparking/model/Ticket.java
+++ src/main/java/sk/telekom/bctparking/model/Ticket.java
@@ -2,10 +2,18 @@ package sk.telekom.bctparking.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 
 import java.time.OffsetDateTime;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Accessors(chain = true)
 public class Ticket {
 
     @Id
